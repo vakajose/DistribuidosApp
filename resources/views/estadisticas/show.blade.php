87a552af06bf276d625d3bd4e2e227f787b360e1
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2018-11-05 10:03:14 -0400

    Config Distri

---
+++ resources/views/estadisticas/show.blade.php
@@ -0,0 +1,24 @@
+@extends('adminlte::page')
+
+@section('title', 'Estadisticas')
+
+@section('content_header')
+    <h1>Estadisticas de encuentas</h1>
+@stop
+
+@section('content')
+
+<form action="" method="POST" role="form">
+	<legend>Form title</legend>
+
+	<div class="form-group">
+		<label for="">label</label>
+		<input type="text" class="form-control" id="" placeholder="Input field">
+	</div>
+
+	
+
+	<button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('crearencuesta', 'EncuestaController@crearencuestaview');
 Route::get('dashboard', 'EncuestaController@dashboard')->name('dashboard');
 Route::post('crearencuesta', 'EncuestaController@crearencuesta');
-
-
 Route::get('getencuesta','WebServiceController@getEncuesta');
 Route::get('getlista','WebServiceController@getLista');
-
-
 Route::post('setRespuestaEncuesta','WebServiceController@setRespuestaEncuesta');
+//
+Route::name('estadisticas')->get('estadisticas','EstadisticasController@show');
