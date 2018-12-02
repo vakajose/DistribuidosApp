b17d23f52e5702ae806f92b6e1f78e0724e86252
Jose Luis Vaca Fernandez <vakajose@gmail.com>
2018-12-02 17:14:25 -0400

    FUNCIONAL al 100%

--- public/js/reclamoMap.js
+++ public/js/reclamoMap.js
@@ -29,7 +29,6 @@ $(function () {
         var infowindow = new google.maps.InfoWindow({
           content: getInfoString(ubi),
         });
-  
 
         var marker = new google.maps.Marker({
           position: location,
@@ -37,11 +36,11 @@ $(function () {
           label: ubi['user_id'].toString(),
           title: ubi['created_at'],
         });
-        markers.push(marker);
-        
+
          marker.addListener('click', function() {
           infowindow.open(marker.get('map'), marker);
         });
+        markers.push(marker);
       }
     
       function getInfoString(ubi) {
@@ -50,13 +49,12 @@ $(function () {
       }
 
   function actualizarMapa(){
-
     $.ajax({
       url: 'getusers'
     }).done(function(users){
        $.ajax({
-          url:'getubicaciones/online'}).done(function(ubicaciones) {
-            
+          url:'getubicaciones/online'
+        }).done(function(ubicaciones){
           var jusers = JSON.parse(users);
           var jubicaciones = JSON.parse(ubicaciones);
           jusers.forEach( function(usr, index) {
@@ -65,15 +63,46 @@ $(function () {
               $('#estado-'+usr['id']).removeClass('offline');
               $('#estado-'+usr['id']).removeClass('suspend');
               $('#estado-'+usr['id']).addClass(usr['estado']);
-          });
-          markers = [];
+            });
+          
+
+          deleteMarkers();
           jubicaciones.forEach( function(ubi, index) {
+
             var marker = {lat: parseFloat(ubi['latitud']), lng: parseFloat(ubi['longitud'])};
             addMarker(marker,ubi);
+            
           });
-          console.log("actualizado")
+
+          console.log("actualizado");
        });
+
     }).fail(function(data) {
       console.log(data);
     });
+
+
   }
+
+  // Sets the map on all markers in the array.
+      function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
+        }
+      }
+
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setMapOnAll(null);
+      }
+
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setMapOnAll(map);
+      }
+
+      // Deletes all markers in the array by removing references to them.
+      function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+      }
